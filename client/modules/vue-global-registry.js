a7f6ea11273a0a45cbd1807d1f8918bacdeab2f5
Hugo Gu <hugogu@outlook.com>
2026-05-17 13:57:28 +0800

    fix(frontend): restore async theme component registration

--- client/modules/vue-global-registry.js
+++ client/modules/vue-global-registry.js
@@ -56,8 +56,8 @@ export function createLegacyAppComponentRegistry (getThemeComponentLoader) {
     { name: 'VCardChin', loader: () => import(/* webpackPrefetch: true, webpackChunkName: "ui-extra" */ '../components/common/v-card-chin.vue') },
     { name: 'VCardInfo', loader: () => import(/* webpackPrefetch: true, webpackChunkName: "ui-extra" */ '../components/common/v-card-info.vue') },
     { name: 'Welcome', loader: () => import(/* webpackChunkName: "welcome" */ '../components/welcome.vue') },
-    { name: 'NavFooter', loader: getThemeComponentLoader('nav-footer')() },
-    { name: 'Page', loader: getThemeComponentLoader('page')() }
+    { name: 'NavFooter', loader: () => getThemeComponentLoader('nav-footer')() },
+    { name: 'Page', loader: () => getThemeComponentLoader('page')() }
   ]
 }
 
