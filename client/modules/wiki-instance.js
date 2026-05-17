c08f899a773aaf203622a5ca912521b17996f0f9
Hugo Gu <hugogu@outlook.com>
2026-05-17 14:01:03 +0800

    refactor(frontend): internalize wiki instance state

--- client/modules/wiki-instance.js
+++ client/modules/wiki-instance.js
@@ -1,12 +1,16 @@
+let currentWikiInstance = null
+
 export function getWikiInstance () {
-  return window.WIKI || null
+  return currentWikiInstance
 }
 
 export function setWikiInstance (instance) {
+  currentWikiInstance = instance
   window.WIKI = instance
   return instance
 }
 
 export function clearWikiInstance () {
+  currentWikiInstance = null
   window.WIKI = null
 }
